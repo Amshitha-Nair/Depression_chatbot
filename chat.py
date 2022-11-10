e31e7719ac6e4aa37e81c19f954ae6ffb3ce02ac
Atharvi Nair <82900813+atharvi06@users.noreply.github.com>
2022-11-10 14:56:01 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     print("Let's chat! (type 'quit' to exit)")
     while True:
         # sentence = "do you use credit cards?"
-        sentence = input("You: ")
+        sentence = input("Me: ")
         if sentence == "quit":
             break
 
